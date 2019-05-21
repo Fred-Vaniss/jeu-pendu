9a471df9b52ad816824619a747e7c2b2f1a37f93
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-05-21 12:07:32 +0200

    Approfondissement de l'exercice
    Le mot à deviner sera juste un string.
    Ce mot sera convertis en tableau
    Le jeu vérifiera si la lettre à déjà été inséré
    Le jeu dira à la fin si le joueur à gagné, perdu ou a annulé sa partie.

--- pendu.js
+++ pendu.js
@@ -1,33 +1,52 @@
 console.clear();
-
 let log = console.log
-const mot = ["B","O","N","J","O","U","R"];
-let pendu = ["_","_","_","_","_","_","_"];
-let found = 0;
-let chance = 7;
-let stringPendu = ""
 
-while (found != mot.length && chance > 0) {
+const mot = "Frederick"    // Variable du mot à trouver
+let motArray = [];      // Tableau vide du mot
+let pendu = [];         // Tableau vide de l'affichage du pendu
+
+for(i = 0; i < mot.length; i++) {
+    motArray.push(mot[i].toUpperCase())   // Conversion du mot en éléments du tableau séparé
+    pendu.push("_")         // Création de cases vide pour les lettres trouvés
+}
+
+let found = 0;      // Compteur de lettres trouvés
+let chance = 7;     // Compteur de chances
+let inputs = []     // Tableau des lettres déjà insérés
+
+while (found != motArray.length && chance > 0) {
+    let stringPendu = ""    // Lettres trouvés à afficher dans le prompt
     for(i = 0; i < pendu.length; i++) {
-        stringPendu = stringPendu + mot[i] + " "
+        stringPendu = stringPendu + pendu[i] + " "  // Insertion des lettres trouvés dans le string
     }
-
 
     let letter = prompt(`Choisi une lettre, il te reste ${chance} essais.\n${stringPendu}`);
     console.log(letter)
 
     if(letter == null){
-        break;
+        break;      // Arret de la boucle si le boutton "annuler" à été appuyé
     }
 
-    for(i = 0; i < mot.length; i++) {
-        if (mot[i] == letter){
+    for(i = 0; i < motArray.length; i++) {
+        if (inputs.indexOf(letter) > -1){
+            console.error('Cette lettre à déjà été inséré')
+            break;
+        } else if (motArray[i] == letter){
             pendu[i] = letter;
             found++
             chance++
         }
     }
 
+    inputs.push(letter)
     chance--
     log(pendu);
+}
+
+if(found == motArray.length){
+    alert("Vous avez gagné!")
+} else if (chance == 0) {
+    alert("Vous avez perdu!")
+} else {
+    alert("Partie annulée")
 }
